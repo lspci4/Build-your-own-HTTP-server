695f4ff76d12964aab2df5484ade520439b942ca
lspci4 <ingluispt@gmail.com>
2025-04-14 01:26:27 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ import socket  # noqa: F401
 
 def main():
     print("Logs from your program will appear here!")
-
-
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221))
     server_socket.accept() 
 
 if __name__ == "__main__":
