4b07e65f40d5e62fdde0fe9fc7c43497f8045bd4
lspci4 <ingluispt@gmail.com>
2025-04-14 09:00:04 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket  # noqa: F401
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221))
-    server_socket.accept() 
-
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
 if __name__ == "__main__":
     main()
