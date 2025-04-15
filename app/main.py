d9b6c5ec895ebcd22aeb1e400010d62f6511d3f6
lspci4 <ingluispt@gmail.com>
2025-04-15 01:45:40 -0400

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -7,19 +7,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     s.listen(1)
     print('Esperando conexión...')
-    conn, addr = s.accept()
-    with conn:
-        print(f'Recibiendo conexión desde', addr)
-        data = conn.recv(1024)
-        print(data.decode())
-        request_data =data.decode().splitlines()
-        print(request_data)
-        request_line = request_data[0].split()
-        print(request_line)
-        if request_line[1]=='/':
-            conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
-        else:
-            conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f'Recibiendo conexión desde', addr)
+            data = conn.recv(1024)
+            print(data.decode())
+            request_data =data.decode().splitlines()
+            print(request_data)
+            request_line = request_data[0].split()
+            print(request_line)
+            if request_line[1]=='/':
+                conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+            else:
+                conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
         
         
