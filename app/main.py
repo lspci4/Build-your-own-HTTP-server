4639b133f1b955e5e20a7c1410aca22d2edb15f5
lspci4 <ingluispt@gmail.com>
2025-04-14 23:51:31 -0400

    pass 2st stage

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221))
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     
 if __name__ == "__main__":
     main()
