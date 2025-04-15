50cfb0c55967804b7ccd3eedd4794743ab2c9bd3
lspci4 <ingluispt@gmail.com>
2025-04-15 01:50:35 -0400

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ host='localhost'
 port=4221
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.listen(1)
     print('Esperando conexión...')
