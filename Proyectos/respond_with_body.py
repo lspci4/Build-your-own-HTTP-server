964378f0713f46c9fd91f7152f11b7a87e968869
lspci4 <ingluispt@gmail.com>
2025-04-16 13:30:24 -0400

    Read header

--- Proyectos/respond_with_body.py
+++ Proyectos/respond_with_body.py
@@ -1,47 +1,48 @@
 import socket
 import re
 
-host='localhost'
-port=4221
+HOST = 'localhost'
+PORT = 4221
+BUFFER_SIZE = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
-    s.listen(1)
-    print('Esperando conexión...')
+def request_user(data): 
+    request_data = data.decode().splitlines()
+    print(request_data) # [A,B,C,D] --> [0,1,2,3]
     
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            data = conn.recv(1024)
-            request_data =data.decode().splitlines()
-            request_line = request_data[0].split()
-            print(request_line[1])
-            
-            request_path= re.search(r'^\/echo\/(.+)$', request_line[1])
-            
-            if request_line[1]=='/': 
-                response = (
-                'HTTP/1.1 200 OK\r\n'
-                'Content-Type: text/plain\r\n'
-                f'Content-Length: {len(request_line[1])}\r\n'
-                '\r\n'
-                f'{request_line[1]}')
-                
-                conn.sendall(response.encode())
-            else:         
-                if request_path or request_line[1]=='/':
-                    response = (
-                    'HTTP/1.1 200 OK\r\n'
-                    'Content-Type: text/plain\r\n'
-                    f'Content-Length: {len(request_path.group(1))}\r\n'
-                    '\r\n'
-                    f'{request_path.group(1)}')
-                    
-                    conn.sendall(response.encode())
-                else:
-                    conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+    for line in request_data[1:]:
+        if line.lower().startswith('user-agent'):
+            parts = line.split(':', 1)
+            user_agent = parts[1].strip()
+            user_agent_len = len(user_agent)
+            print(user_agent,user_agent_len)
+    return user_agent, user_agent_len
 
+def response_user(user_agent, user_agent_len):
+    content = (
+        f'HTTP/1.1 200 OK\r\n',
+        f'Content-Type: text/plain\r\n',
+        f'Content-Length: {user_agent_len}\r\n',
+        f'\r\n',
+        f'{user_agent}'
+    )
+    return ''.join(content)
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
         
+        with conn:
+            print(f'Server listening on {HOST}:{PORT}')
+            while True:
+                data = conn.recv(BUFFER_SIZE)
+                if data:
+                    user_agent, user_agent_len = request_user(data)
+                    http_response = response_user(user_agent,user_agent_len)
+                    print(http_response.encode())
+                    conn.sendall(http_response.encode())
         
-        
+if __name__=="__main__":
+    main()
