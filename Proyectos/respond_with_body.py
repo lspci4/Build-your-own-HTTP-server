0dc89570b5a8ed36cfa210fc719204540f7fef98
lspci4 <ingluispt@gmail.com>
2025-04-16 01:01:12 -0400

    Respond with body

---
+++ Proyectos/respond_with_body.py
@@ -0,0 +1,47 @@
+import socket
+import re
+
+host='localhost'
+port=4221
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(1)
+    print('Esperando conexión...')
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            data = conn.recv(1024)
+            request_data =data.decode().splitlines()
+            request_line = request_data[0].split()
+            print(request_line[1])
+            
+            request_path= re.search(r'^\/echo\/(.+)$', request_line[1])
+            
+            if request_line[1]=='/': 
+                response = (
+                'HTTP/1.1 200 OK\r\n'
+                'Content-Type: text/plain\r\n'
+                f'Content-Length: {len(request_line[1])}\r\n'
+                '\r\n'
+                f'{request_line[1]}')
+                
+                conn.sendall(response.encode())
+            else:         
+                if request_path or request_line[1]=='/':
+                    response = (
+                    'HTTP/1.1 200 OK\r\n'
+                    'Content-Type: text/plain\r\n'
+                    f'Content-Length: {len(request_path.group(1))}\r\n'
+                    '\r\n'
+                    f'{request_path.group(1)}')
+                    
+                    conn.sendall(response.encode())
+                else:
+                    conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+
+        
+        
+        
